d08bfd4d0388b4c79fadd56506850ef0be2e0689
David Desmaisons <desmaisons_david@hotmail.com>
2018-07-23 14:12:09 -0300

    fix template adjust

--- generator/template/src/App.vue
+++ generator/template/src/App.vue
@@ -1,5 +1,5 @@
 ---
 extend: '@vue/cli-service/generator/template/src/App.vue'
-replace: !!js/regexp /import HelloWorld from "\.\/components\/HelloWorld\.vue";/
+replace: !!js/regexp /import HelloWorld from "\.\/components\/HelloWorld\.vue"/
 ---
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/HelloWorld.vue"
