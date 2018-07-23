e756f5773103601edbe24d8007e40378e1b2fb18
desmaisons_david <desmaisons_david@hotmail.com>
2018-07-23 00:43:11 -0300

    WIP

--- generator/index.js
+++ generator/index.js
@@ -4,28 +4,28 @@ module.exports = (api, config) => {
     scripts: {
       serve: "vue-cli-service serve ./example/main.js --open",
       build: "vue-cli-service build --target lib ./src/index.js",
-    }
+    },
+    private: false,
+    files: [
+      "dist/*.css",
+      "dist/*.map",
+      "dist/*.js",
+    ]
   })
 
-  api.render('./template', {
-    isTest: process.env.VUE_CLI_TEST || process.env.VUE_CLI_DEBUG,
-    hasMocha,
-    hasJest
-  })
+  api.render('./template')
 
   api.postProcessFiles(files => {
+    const sourceFiles = /^src\//
+    const immutableFiles = ['src/components/HelloWord.vue', 'src/index.js']
+
     for (const file in files) {
-      if (jsRE.test(file) && !excludeRE.test(file)) {
-        const tsFile = file.replace(jsRE, '.ts')
-        if (!files[tsFile]) {
-          let content = files[file]
-          if (tsLint) {
-            content = convertLintFlags(content)
-          }
-          files[tsFile] = content
-        }
-        delete files[file]
+      if (!sourceFiles.test(file) || immutableFiles.indexOf(file) !== -1) {
+        continue;
       }
+      const migratedFile = file.replace(sourceFiles, 'example/');
+      files[migratedFile] = files[file];
+      delete files[file];
     }
   })
 }
